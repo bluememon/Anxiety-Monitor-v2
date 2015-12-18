29ea8b7f12eb1093dbe31e14abf0488cfd20e5e1
Guillermo Uribe <bluememon@gmail.com>
2015-12-18 14:34:51 -0600

    Agregado calendario custom para el dasa calendar y tambien un poco de codigo muestra para empezar a colorear las fechas

--- app/src/main/java/layout/dasa_calendar.java
+++ app/src/main/java/layout/dasa_calendar.java
@@ -7,19 +7,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
 
 import com.bluecoreservices.anxietymonitor2.R;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class dasa_calendar extends Fragment {
     public final static String PAGINA_DEBUG = "add_dasa";
 
     View view;
+    OnDateSelectedListener listener;
 
-    public CalendarView calendario;
+    public MaterialCalendarView calendario;
 
     public dasa_calendar() {
         // Required empty public constructor
@@ -36,13 +39,29 @@ public class dasa_calendar extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_dasa_calendar, container, false);
 
-        calendario = (CalendarView) view.findViewById(R.id.calendarView);
-        SimpleDateFormat fecha = new SimpleDateFormat("dd/MM/yyyy");
-        String fechaSeleccionada =  fecha.format(new Date(calendario.getDate()));
+        calendario = (MaterialCalendarView) view.findViewById(R.id.calendarView);
 
-        Log.i(PAGINA_DEBUG, fechaSeleccionada);
+        initializeCalendar();
 
         return view;
+    }
+
+    private void initializeCalendar() {
+
+        calendario.setOnDateChangedListener(listener);
+        calendario.setShowOtherDates(MaterialCalendarView.SHOW_ALL);
+
+        Calendar calendar = Calendar.getInstance();
+        calendario.setSelectedDate(calendar.getTime());
+
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.JANUARY, 1);
+        calendario.setMinimumDate(calendar.getTime());
+
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.DECEMBER, 31);
+        calendario.setMaximumDate(calendar.getTime());
+
+        /*int bgColor = sharedVisualElements.getPrimaryColor();
+        calendario.addDecorators(new EventDecorator(bgColor, ????));*/
     }
 
 }
